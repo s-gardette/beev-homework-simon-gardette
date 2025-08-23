85b6f84e982b9fbe1374a8c3d84c86e4558964a5
Simon Gardette <simon.gardette@syslex.fr>
2025-08-23 20:31:32 +0200

    feat: Make the docker compose and services ready for a minimal deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.DOMAIN ? `https://${process.env.DOMAIN}` : 'http://localhost:3000',
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     }
